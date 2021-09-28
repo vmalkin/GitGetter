1d6ba10b84007db649a71c82476d011bba769680
vmalkin <vaughnmalkin@gmail.com>
2021-09-29 09:36:14 +1300

    reformat txt output for commits info

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ with open("../git.token") as g:
 g = Github(git_token)
 
 repo_names = [
-    # "BIT-Studio-2/project-21s2-buddy-on-the-beach",
-    # "BIT-Studio-2/project-21s2-ark-tech",
-    # "BIT-Studio-2/project-21s2-beach-boys",
-    # "BIT-Studio-2/project-21s2-beach-buddy",
-    # "BIT-Studio-2/project-21s2-jackal",
-    # "BIT-Studio-2/project-21s2-paw-patrol",
-    # "BIT-Studio-2/project-21s2-sea-dogs",
+    "BIT-Studio-2/project-21s2-buddy-on-the-beach",
+    "BIT-Studio-2/project-21s2-ark-tech",
+    "BIT-Studio-2/project-21s2-beach-boys",
+    "BIT-Studio-2/project-21s2-beach-buddy",
+    "BIT-Studio-2/project-21s2-jackal",
+    "BIT-Studio-2/project-21s2-paw-patrol",
+    "BIT-Studio-2/project-21s2-sea-dogs",
     "BIT-Studio-2/project-21s2-walkeez"
     ]
 
@@ -31,6 +31,32 @@ def display_group_members(repo):
     for user in users:
         peeps = str(user) + " " + str(user.last_modified) + " " + str(user.name)
         print(peeps)
+
+
+def display_summary_commits(repo):
+    tally = []
+    # First get all branches for the repo
+    branches = repo.get_branches()
+    for branch in branches:
+        commits = repo.get_commits(branch.name)
+        try:
+            for commit in commits:
+                if commit.author is None:
+                    author = "INVALID CONTRIBUTOR"
+                else:
+                    author = commit.author.login
+
+                tally.append(author)
+        except github.GithubException:
+            print("Error: no commits")
+
+    print("### TOTAL COMMITS BY CONTRIBUTORS ACROSS ALL BRANCHES")
+
+    commit_summary = Counter(tally)
+    print("| Group Member | No of Commits |")
+    print("| --- | --- |")
+    for keys, values in commit_summary.items():
+        print("| " + keys + " | " + str(values) + " |")
 
 
 def display_commits_all_branches(repo):
@@ -52,8 +78,6 @@ def display_commits_all_branches(repo):
                 else:
                     author = commit.author.login
 
-                tally.append(author)
-
                 linkname = "[" + author + "]"
                 linkurl = "(" + commit.html_url + ")"
                 url = linkname + linkurl
@@ -65,9 +89,6 @@ def display_commits_all_branches(repo):
         sorted_commits = sorted(c, key=lambda c: (c[2], c[0]))
         for cm in sorted_commits:
             print(cm)
-
-    print("### TOTAL COMMITS BY CONTRIBUTORS")
-    print(Counter(tally))
 
 
 def display_member_commit_times(repo):
@@ -125,9 +146,9 @@ def plot_frequency(team_name, times, names):
 
 
 if __name__ == '__main__':
-    # project_start = "2020-09-13"
-    # project_start_posix = posix2utc(project_start, '%Y-%m-%d')
-    # project_time_now = time()
+    project_start = "2020-09-13"
+    project_start_posix = posix2utc(project_start, '%Y-%m-%d')
+    project_time_now = time()
 
     print("Convert Markdown to HTML: https://dillinger.io/")
     for name in repo_names:
@@ -139,17 +160,19 @@ if __name__ == '__main__':
         repo = g.get_repo(name)
 
         display_group_members(repo)
+        display_summary_commits(repo)
+
         display_commits_all_branches(repo)
-        plotdata = display_member_commit_times(repo)
+        # plotdata = display_member_commit_times(repo)
         # plot_frequency(name, plotdata[0], plotdata[1])
-
-        print("---")
         print("END")
+        print("---")
 
-    # time_end = time()
-    # print("Finished!")
-    # project_elapsed_days = round((project_time_now - project_start) / 86400, 1)
-    # print("Project has been running for " + project_elapsed_days + " days")
+
+    time_end = time()
+    print("Finished!")
+    project_elapsed_days = round((project_time_now - project_start) / 86400, 1)
+    print("Project has been running for " + project_elapsed_days + " days")
 
 
 
