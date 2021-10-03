18ebc54b2429894b1e2970a4a828f72fd7c68525
Vaughn Malkin <vaughnmalkin@gmail.com>
2021-10-04 10:28:26 +1300

    tidy up main after creating helper modules

--- manager_time.py
+++ manager_time.py
@@ -1,13 +1,14 @@
-def posix_to_nzst(timestring):
-    # Timestring is GMT
-    timeformat = '%a, %d %b %Y %H:%M:%S GMT'
+from datetime import datetime
 
 
 def gmt_to_posix(timestring):
     # Timestring is GMT
     timeformat = '%a, %d %b %Y %H:%M:%S GMT'
 
+def utc2posix():
+    pass
+
 def posix2utc(posixtime, timeformat):
     # '%Y-%m-%d %H:%M'
-    utctime = datetime.datetime.utcfromtimestamp(int(posixtime)).strftime(timeformat)
+    utctime = datetime.utcfromtimestamp(int(posixtime)).strftime(timeformat)
     return utctime
